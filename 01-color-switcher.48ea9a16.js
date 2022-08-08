dc1116ed2e6fc68d4b977e7a521e60d0b59f82cc
Varden41 <105563413+Varden41@users.noreply.github.com>
2022-08-08 15:39:40 +0000

    Deploying to gh-pages from @ Varden41/goit-js-hw-09@750e05360fac4dc27c06ee3b3e7c8da46955a52e 🚀

---
+++ 01-color-switcher.48ea9a16.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.disabled=!0}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}));var o=null}();
+//# sourceMappingURL=01-color-switcher.48ea9a16.js.map
