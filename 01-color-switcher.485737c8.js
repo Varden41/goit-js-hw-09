b09d25931ec1eb880c0fd66625ccbfdb0ab41674
Varden41 <105563413+Varden41@users.noreply.github.com>
2022-08-08 12:39:25 +0000

    Deploying to gh-pages from @ Varden41/goit-js-hw-09@83a9c06dd542bc2d5efa96c0d9ab5f1a4237fac2 🚀

---
+++ 01-color-switcher.485737c8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){timerId=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.disabled=!0}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.485737c8.js.map
---
+++ 01-color-switcher.bf29b3af.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");t.addEventListener("click",(function(){timerId=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.disabled=!0}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.bf29b3af.js.map
